ac49302e95f4cb5376c925acc6ed874facb75784
cs60181888 <qwerkdud1999@gmail.com>
2020-09-26 08:19:52 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import TOC from './components/TOC';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
+import Subject from './components/Subject';
+import Control from './components/Control'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.max_content_id = 3;
+    this.state = {
+      mode:'welcome',
+      subject:{title:'WEB', sub:'world wide web!'},
+      welcome:{title:'Welcome', desc:'Hello World!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JAVASCRIPT', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+
+  getReadContent(){
+    var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          return data;
+          break;
+        }
+        i = i+1;
+      }
+  }
+
+  getContent(){
+    var _title, _desc, _article = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'read'){
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content._title} desc={_content._desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        this.max_content_id = this.max_content_id + 1;
+        var _contents = this.state.contents.concat(
+          ({id:this.max_content_id, title:_title, desc:_desc})
+        )
+        this.setState({ contents:_contents });
+      }.bind(this)}>
+      </CreateContent>
+    } else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc){
+        this.max_content_id = this.max_content_id + 1;
+        var _contents = this.state.contents.concat(
+          ({id:this.max_content_id, title:_title, desc:_desc})
+        )
+        this.setState({ contents:_contents });
+      }.bind(this)}>
+      </UpdateContent>
+    }
+    return _article;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub} onChangePage={function(){
+          this.setState({mode:'welcome'})
+        }}></Subject>
+        <TOC onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          })
+        }.bind(this)} data={this.state.contents}>
+        </TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({mode:_mode});
+        }.bind(this)}></Control>
+        {this.getContent()}
+      </div>
+    );
+  }
 }
 
 export default App;
